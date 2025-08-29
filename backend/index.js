ac9ff01d79cda6e3c0e5a407fac5f34923fd66ef
aniket sharma <aniket220603@gmail.com>
2025-08-30 00:12:30 +0530

    added frontend url

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,14 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import geminiRoutes from "./routes/aiRoutes.js";
 import murfRoutes from "./routes/murf.js";
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
 dotenv.config();
+const Frontend_URL = process.env.FRONTEND_URL;
+const app = express();
+app.use(cors({
+    origin : Frontend_URL
+}));
+app.use(bodyParser.json());
+
 
 app.use("/api/ai", geminiRoutes);
 app.use("/api/murf", murfRoutes);
